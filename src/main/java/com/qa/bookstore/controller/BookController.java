471bb33ffbbbe25485114c3920733b32cb76c71c
gregory <gregory.evans@hotmail.co.uk>
2022-04-20 18:15:43 +0100

    application properties added

--- src/main/java/com/qa/bookstore/controller/BookController.java
+++ src/main/java/com/qa/bookstore/controller/BookController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.qa.bookstore.domain.Book;
+import com.qa.bookstore.service.BookService;
 
 @RestController
 public class BookController {
