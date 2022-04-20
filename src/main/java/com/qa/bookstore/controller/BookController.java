b5f521a176ed320f55fb915b5f52a4f350f3035a
gregory <gregory.evans@hotmail.co.uk>
2022-04-20 18:01:48 +0100

    domain book created

--- src/main/java/com/qa/bookstore/controller/BookController.java
+++ src/main/java/com/qa/bookstore/controller/BookController.java
@@ -13,6 +13,8 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.qa.bookstore.domain.Book;
+
 @RestController
 public class BookController {
 	private BookService service;
